45a8f8be11f6e43578e62ca691881864dccfc779
Harrison Bouche <harrison@sm.media>
2023-04-18 12:07:25 -0400

    Added donation button

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -12,6 +12,16 @@ const handleMenuKey = (event: KeyboardEvent) => {
     }
 }
 
+let donationOpen = false
+const handleDonation = () => {
+    donationOpen = !donationOpen
+}
+const handleDonationKey = (event: KeyboardEvent) => {
+    if (event.key === "Space" || event.key === "Enter") {
+        handleDonation()
+    }
+}
+
 
 // Functionality for bg color switch on scroll
 let atTop: boolean = true
@@ -45,7 +55,7 @@ const handleScroll = () => {
     <div class="
             w-full px-4 sm:px-8 md:px-16
             relative z-20
-            flex justify-between items-center gap-8
+            flex justify-between items-center md:items-start gap-8
             transition-all duration-500 ease-[cubic-bezier(.61, .16, .07, 1.46)]
             {atTop ? 'pt-10 md:pt-16' : 'py-4'}">
         <div class="flex items-center gap-8">
@@ -102,28 +112,69 @@ const handleScroll = () => {
                     rounded-full border-none
                     {atTop ? 'bg-white' : 'bg-upls-orange'}" />
         </div>
-        <div class="hidden md:flex items-center gap-4">
-            <a href="https://facebook.com/uplionsserve" class="w-8 fill-upls-orange {atTop ? 'brightness-0 invert' : 'brightness-100 invert-0'}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>
-            </a>
-            <a href="https://instagram.com/uplionsserve" class="w-8 fill-upls-orange {atTop ? 'brightness-0 invert' : 'brightness-100 invert-0'}">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
-            </a>
+        <div class="md:pt-2 hidden md:flex items-start gap-4">
+                <a href="https://facebook.com/uplionsserve" class="w-8 fill-upls-orange pt-[0.8rem] {atTop ? 'brightness-0 invert' : 'brightness-100 invert-0'}">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg>
+                </a>
+                <a href="https://instagram.com/uplionsserve" class="w-8 fill-upls-orange pt-3 {atTop ? 'brightness-0 invert' : 'brightness-100 invert-0'}">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                </a>
+            <div class="
+                    hidden md:flex w-full 
+                    flex-col justify-end items-end
+                    grow">
+                <div class="
+                        w-full max-w-xs md:w-44
+                        rounded-md
+                        text-center
+                        cursor-pointer
+                        transition-all 
+                        {atTop ? 'bg-white text-upls-orange' : 'bg-upls-orange text-white'}">
+                    <div 
+                        on:keypress={handleDonation}
+                        on:click={handleDonation}
+                        class="py-3">
+                        <span class="">Donate</span>
+                        <span class="
+                            inline-block
+                            transition-all duration-200 ease-[cubic-bezier(.61, .16, .07, 1.46)]
+                            {donationOpen ? 'rotate-[135deg] translate-y-[3.5px]' : '-rotate-45 -translate-y-[2.5px]'}">&#x221F;</span>
+                    </div>
+                    <div class="
+                            overflow-hidden
+                            transition-all duration-300
+                            {donationOpen ? 'h-44 pb-8' : 'h-0 pb-0'}">
+                        <div class="mx-2 bg-white rounded-md">
+                            <a
+                                href="https://service.thrivent.com/apps/ThriventChoiceMemberWeb/public/orgDetails/100864474940"
+                                class="block py-8 px-6">
+                                <img src="/graphics/brands/logo-thrivent.svg" alt="Thrivent logo" />
+                            </a>
+                            <hr class="mx-8 border-[rgba(0,0,0,0.075)]" />
+                            <a
+                                href="https://causes.benevity.org/"
+                                class="block py-[1.125rem] px-4">
+                                <img src="/graphics/brands/logo-benevity.svg" alt="Benevity logo"/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <menu class="
-        w-full pt-32 pr-4 md:w-auto md:pt-0
+        w-full h-screen md:h-auto pt-32 pr-4 md:w-auto md:pt-0
         fixed top-0 -z-10 md:static md:top-auto md:z-0
         flex justify-end md:block
         {!menuOpen ? 'invisible md:visible' : 'visible'}">
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <div on:click={handleMenu} class="
-                w-screen h-screen
+                w-screen h-[100svh]
                 fixed top-0 left-0 -z-30
                 transition-all duration-300 ease-[cubic-bezier(.61, .16, .07, 1.46)]
                 {!menuOpen ? 'backdrop-blur-none brightness-100 md:invisible' : 'backdrop-blur-md brightness-90'}"></div>
         <ul class="
-                md:pl-[17.25rem]
+                w-full md:w-auto h-full md:h-auto pl-4 md:pl-[17.25rem] pb-4 md:pb-0
                 relative z-30 md:absolute md:top-0 left-0
                 flex flex-col gap-6 items-end md:gap-2 md:items-start
                 text-2xl font-bold md:text-lg
@@ -134,6 +185,46 @@ const handleScroll = () => {
             <a on:click={handleMenu} href={import.meta.env.PUBLIC_DEFENSEAGAINSTDIABETES_URL}>Defense Against Diabetes</a>
             <a on:click={handleMenu} href={import.meta.env.PUBLIC_PROJECTKIDSIGHT_URL}>Project Kidsight</a>
             <a on:click={handleMenu} href={import.meta.env.PUBLIC_HUNGER_URL}>D10 Hunger</a>
+            <div class="
+                    md:hidden w-full 
+                    flex flex-col justify-end items-end
+                    grow">
+                <div class="
+                        w-full max-w-xs py-4 px-6 sm:py-0 sm:pb-2
+                        bg-white rounded-md
+                        text-upls-orange text-center
+                        cursor-pointer">
+                    <div class="
+                            overflow-hidden
+                            transition-all duration-300
+                            {donationOpen ? 'h-52 pb-8' : 'h-0 pb-0'}">
+                        <div class="
+                                transition-all duration-200">
+                            <a
+                                href="https://service.thrivent.com/apps/ThriventChoiceMemberWeb/public/orgDetails/100864474940"
+                                class="block py-8 px-16">
+                                <img src="/graphics/brands/logo-thrivent.svg" alt="Thrivent logo" />
+                            </a>
+                            <hr class="mx-8 border-[rgba(0,0,0,0.075)]" />
+                            <a
+                                href="https://causes.benevity.org/"
+                                class="block py-6 px-12">
+                                <img src="/graphics/brands/logo-benevity.svg" alt="Benevity logo"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div 
+                        on:keypress={handleDonation}
+                        on:click={handleDonation}
+                        class="pt-6 pb-4">
+                        <span class="">Donate</span>
+                        <span class="
+                            inline-block
+                            transition-all duration-200 ease-[cubic-bezier(.61, .16, .07, 1.46)]
+                            {donationOpen ? 'rotate-[135deg] translate-y-[3.5px]' : '-rotate-45 -translate-y-[2.5px]'}">&#x221F;</span>
+                    </div>
+                </div>
+            </div>
         </ul>
     </menu>
 </header>
