c65b511f9bff60134a63e913859b41dcafc6ff9f
sugarmaplemedia <harrison@sm.media>
2023-04-26 07:54:39 -0400

    Actually enabled SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
+import vercel from "@astrojs/vercel/serverless";
 
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: 'https://uplionsserve.org',
-  output: "static",
+  output: "server",
   adapter: vercel(),
   integrations: [svelte(), tailwind()],
 });
